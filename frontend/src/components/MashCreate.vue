ac6a1bb6cae4fe1880e1061579b610eed10ed63e
Paul Coignet <paulcoignet1996@gmail.com>
2020-04-13 16:43:05 +0200

    Add question

--- frontend/src/components/MashCreate.vue
+++ frontend/src/components/MashCreate.vue
@@ -1,25 +1,19 @@
 <template>
   <div class="mash-create">
-    <router-link to="/mash/42/result" tag="button">Let's start</router-link> <br />
-    <button @click="startTournament">Test start</button>
+    <button @click="startTournament">Let's start!</button>
     <hr />
     <div class="mash-create-choices">
-      <!-- <picture-input 
-        v-for="(img, index) in images" :key="index" ref="chosen"
-        @change="onChange(index)" 
-        @remove="onRemove(index)"
-        width="130" 
-        height="200" 
-        margin="16" 
-        accept="image/jpeg,image/png,gif" 
-        size="10" 
-        :removable="true"
-        :customStrings="{
-          drag: 'Drag a choice'
-        }">
-      </picture-input>
-      <hr /> -->
-      <vue-dropzone ref="myVueDropzone" id="dropzone" :options="dropzoneOptions"></vue-dropzone>
+      <b-container fluid>
+        <b-row class="my-1">
+          <b-col sm="3">
+            <label>Question: </label>
+          </b-col>
+          <b-col sm="9">
+            <b-form-input v-model="question" type="text" placeholder="What's best?"></b-form-input>
+          </b-col>
+        </b-row>
+        <vue-dropzone ref="dropzone" id="dropzone" :options="dropzoneOptions"></vue-dropzone>
+      </b-container>
     </div>
   </div>
 </template>
@@ -28,6 +22,9 @@
 // import PictureInput from 'vue-picture-input';
 import vue2Dropzone from "vue2-dropzone";
 import 'vue2-dropzone/dist/vue2Dropzone.min.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 import axios from 'axios';
 
 const toBase64 = file => new Promise((resolve, reject) => {
@@ -40,19 +37,18 @@ const toBase64 = file => new Promise((resolve, reject) => {
 export default {
   name: 'MashCreate',
   components: {
-    // PictureInput,
     vueDropzone: vue2Dropzone,
   },
   data() {
     return {
       images: [],
+      question: "",
       dropzoneOptions: {
         url: 'https://httpbin.org/post',
         resizeWidth: 200,
         resizeHeight: 300,
         maxFilesize: 0.5,
         addRemoveLinks: true,
-        headers: { "My-Awesome-Header": "header value" },
       }
     }
   },
@@ -61,31 +57,15 @@ export default {
     this.dropzoneOptions.removedfile = this.removeUploadedFile;
   },
   methods: {
-    // onChange (index) {
-    //   if (index === this.images.length - 1) {
-    //     this.images.push(null);
-    //   }
-    //   if (index >= 0 && index < this.images.length) {
-    //     this.images[index] = this.$refs.chosen[index].file;
-    //   }
-    //   console.log(this.images);
-    // },
-    // onRemove (index) {
-    //   if (index >= 0 && index < this.images.length) {
-    //     this.images.splice(index, 1);
-    //     let i;
-    //     for(i = this.images.length - 2; i >= index; i--) {
-    //       console.log(i);
-    //     }
-    //   }
-    //   console.log(this.images);
-    // },
+    questionChanged(val, oldVal) {
+      console.log(val);
+      console.log(oldVal);
+    },
     async acceptUploadedFile(file, done) {
       this.images.push([file.upload.uuid, file]);
       done();
     },
     removeUploadedFile(file) {
-      // console.log(file.upload.uuid);
       var index;
       for (index = 0; index < this.images.length; index++) {
         if (this.images[index][0] === file.upload.uuid) {
@@ -96,8 +76,11 @@ export default {
       return false;
     },
     async createTournamentRequest() {
+      if (this.question.length == 0 || this.images.length < 2) {
+        return null;
+      }
       let tournament = {
-        "question": "What's best?",
+        "question": this.question,
         "size": this.images.length,
         "type": 1,
         "choices": [],
@@ -116,9 +99,13 @@ export default {
     },
 
     async startTournament() {
-      console.log("In start Tournament");
       if (this.images) {
-        const tournament = await this.createTournamentRequest()
+        const tournament = await this.createTournamentRequest();
+
+        if (tournament == null) {
+          console.log("Add more pictures or ask a question");
+          return;
+        }
   
         console.log(tournament);
         const config = {
