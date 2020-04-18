b466f24dbd48706eb3063f1cc7e6bf969ac00fc3
Paul Coignet <paulcoignet1996@gmail.com>
2020-04-18 12:44:38 +0200

    Change font

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="big_button">
-    <b-row align-h="center" class="my-5 mx-3"><h4>You wanna rank some stuff? Do it here!</h4></b-row>
-    <b-row align-h="center">
-      <b-button variant="outline-primary" to="/mash/create"><h4>Start a tournament</h4></b-button>
-    </b-row>
+    <b-container class="pt-5 justify-content-center">
+      <b-row align-h="center" class="my-5 mx-3"><h4>You wanna rank some stuff? Do it here!</h4></b-row>
+      <b-row align-h="center">
+        <b-button variant="outline-primary" to="/mash/create"><h4>Start a tournament</h4></b-button>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
